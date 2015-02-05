6230881895b8b6237b155e51cc4bc61ff5d2faa6
Praveen Balasubramanian <bpraveen92@gmail.com>
2015-02-05 09:58:28 -0600

    Update run_analysis.R

--- Peer-Assesment/run_analysis.R
+++ Peer-Assesment/run_analysis.R
@@ -17,7 +17,7 @@ joinLabel <- rbind(trainLabel, testLabel)
 dim(joinLabel)
 joinSubject <- rbind(trainSubject, testSubject)
 dim(joinSubject)
-
+#Extracts only the measurements on the mean and standard deviation for each measurement. 
 features <- read.table("./data/features.txt")
 dim(features) 
 meanStdIndices <- grep("mean\\(\\)|std\\(\\)", features[, 2])
@@ -26,9 +26,9 @@ joinData <- joinData[, meanStdIndices]
 dim(joinData)
 names(joinData) <- gsub("\\(\\)", "", features[meanStdIndices, 2])
 names(joinData) <- gsub("mean", "Mean", names(joinData))
-names(joinData) <- gsub("std", "Std", names(joinData)
+names(joinData) <- gsub("std", "Std", names(joinData))
 names(joinData) <- gsub("-", "", names(joinData))
-
+#Uses descriptive activity names to name the activities in the data set
 activity <- read.table("./data/activity_labels.txt")
 activity[, 2] <- tolower(gsub("_", "", activity[, 2]))
 substr(activity[2, 2], 8, 8) <- toupper(substr(activity[2, 2], 8, 8))
@@ -36,13 +36,13 @@ substr(activity[3, 2], 8, 8) <- toupper(substr(activity[3, 2], 8, 8))
 activityLabel <- activity[joinLabel[, 1], 2]
 joinLabel[, 1] <- activityLabel
 names(joinLabel) <- "activity"
-
+#Appropriately labels the data set with descriptive variable names. 
 names(joinSubject) <- "subject"
 cleanedData <- cbind(joinSubject, joinLabel, joinData)
 dim(cleanedData) # 10299*68
 write.table(cleanedData, "merged_data.txt")
 
-
+#From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
 subjectLen <- length(table(joinSubject)) # 30
 activityLen <- dim(activity)[1] # 6
 columnLen <- dim(cleanedData)[2]
